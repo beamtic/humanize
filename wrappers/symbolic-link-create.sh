12b47a00252596dc38102705954e436098e412c0
jacobkri <jacobseated@gmail.com>
2019-12-09 09:51:17 +0100

    Added some friendly comments

--- wrappers/symbolic-link-create.sh
+++ wrappers/symbolic-link-create.sh
@@ -6,6 +6,8 @@
 #
 # script author: JacobSeated
 
+# Function to "unfold" the tilde "~" character so it correctly points
+# to the users home directory. I.e.: /home/MyUser/
 unfold_path () {
   unfoldTilde=~
   unfoldedPath="${stringInput/\~/$unfoldTilde}"
@@ -13,6 +15,7 @@ unfold_path () {
 
 printf "  The destination is the location that you want the link to point to (not the location of the link file).\n"
 printf "  Type the destination File Path below:\n\n"
+# Read the user input, while allowing the user to use tab to complete paths (-e)
 read -e stringInput
 unfold_path
 destinationFilePath=$unfoldedPath
@@ -24,4 +27,5 @@ read -e stringInput
 unfold_path
 sourceFilePath=$unfoldedPath
 
+# Run the ln command with the options for symbolic link creation
 ln -s -T "$destinationFilePath" "$sourceFilePath"
