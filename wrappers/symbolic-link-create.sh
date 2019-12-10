f2b636298a5e8e2f91b8b0134aca60a03fff5a95
jacobkri <jacobseated@gmail.com>
2019-12-10 09:23:18 +0100

    Expand tilde properly, exit codes, and comments.

--- wrappers/symbolic-link-create.sh
+++ wrappers/symbolic-link-create.sh
@@ -6,12 +6,31 @@
 #
 # script author: JacobSeated
 
+#  |======================================|
+#  |=======Dependencies===================|
+#  |======================================|
+if [ ! -x "$(command -v python)" ]
+then
+  printf 'This script requires Python to be installed. Exiting.'
+  # Exit with 2 = Missing keyword or command, or permission problem...
+  # See also: http://www.tldp.org/LDP/abs/html/exitcodes.html
+  # A zero code indicates success, while a value higher than zero indicates an error
+  exit 2
+fi
+
+#  |======================================|
+#  |=======Functions======================|
+#  |======================================|
+
 # Function to "unfold" the tilde "~" character so it correctly points
 # to the users home directory. I.e.: /home/MyUser/
 unfold_path () {
-  unfoldTilde=~
-  unfoldedPath="${stringInput/\~/$unfoldTilde}"
+  unfoldedPath=$(python -c "import os.path;print os.path.expanduser(\"$userInput\")")
 }
+
+#  |>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|
+#  |>>>>>>>Execute>>>>>>>>>>>>>>>>>>>>>>>>|
+#  |>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|
 
 printf "  The destination is the location that you want the link to point to (not the location of the link file).\n"
 printf "  Type the destination File Path below:\n\n"
@@ -29,3 +48,5 @@ sourceFilePath=$unfoldedPath
 
 # Run the ln command with the options for symbolic link creation
 ln -s -T "$destinationFilePath" "$sourceFilePath"
+
+exit
--- wrappers/youtube-dl-askme.sh
+++ wrappers/youtube-dl-askme.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# youtube-dl Script that will ask the user what to download
-# Note. There are likely GUIs for youtube-dl which are better to use. Google "youtube-dl GUI"
-# Script by JacobSeated
+#
+#  youtube-dl wrapper script to simplify downing of Audio and Video from YouTube
+#  Note. There are likely GUIs for youtube-dl which are better to use. Google "youtube-dl GUI"
+#
+# Script author: JacobSeated
 
 echo "Supply the URL for the resource you want to download. I.e.: https://www.youtube.com/watch?v=[VIDEO_ID]"
 read resourceURL
@@ -9,6 +11,8 @@ read resourceURL
 echo "what do you want to download? (V for video - A for audio)"
 read videoAudio
 
+# Check if Audio or Video was selected
+# To keep things simple, mp3 is used for audio
 if [ $videoAudio = 'A' ]
 then
   youtube-dl --extract-audio --audio-format mp3 $resourceURL
@@ -16,3 +20,6 @@ elif [ $videoAudio = 'V' ]
 then
   youtube-dl $resourceURL 
 fi
+
+# Exit with status of last executed command
+exit
